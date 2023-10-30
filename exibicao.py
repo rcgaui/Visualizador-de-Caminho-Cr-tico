692a881b4c53c2b777d1d5114911cc4fed780e5b
Vagner Messias <vagnermessias.vmc@gmail.com>
2023-10-29 22:33:30 -0300

    Leitura CSV

--- exibicao.py
+++ exibicao.py
@@ -2,4 +2,9 @@
 def ErroTXTnotFound(NomeDoArquivo):
     errorMsg = "Desculpe, o arquivo " + NomeDoArquivo + " nao foi encontrado na pasta GeneralData."
     print(errorMsg)
-    
+
+
+
+def ErroCSVnotFound(NomeDoArquivo):
+    errorMsg = "Desculpe, o arquivo " + NomeDoArquivo + " nao foi encontrado na pasta GeneralData."
+    print(errorMsg)
--- leituradados.py
+++ leituradados.py
@@ -1,19 +1,20 @@
 import os
 from exibicao import *
+import csv
 
-def LerArquivoTexto(NomeDoArquivo):
+def LerArquivoTexto(nomeDoArquivo):
     dirPath = os.path.dirname(os.path.abspath(__file__))
     folderPath = 'GeneralData'
     filePath = os.path.join(dirPath, folderPath)
-    filePath = os.path.join(filePath, NomeDoArquivo)
+    filePath = os.path.join(filePath, nomeDoArquivo)
     contadorLinhas = 0
 
     try:
         with open(filePath, "r") as file:
             cronograma = []
-            contadorLinhas += 1
 
             for task in file:
+                contadorLinhas += 1
                 tasksArray = task.split()
                 if len(tasksArray) != 5:
                     raise Exception("A quantidade de campos preenchidos na linha " + str(contadorLinhas) +  " esta incorreta")
@@ -28,13 +29,43 @@ def LerArquivoTexto(NomeDoArquivo):
                 
                 cronograma.append(tasksArray)
     except FileNotFoundError:
-        ErroTXTnotFound(NomeDoArquivo=NomeDoArquivo)
+        ErroTXTnotFound(NomeDoArquivo=nomeDoArquivo)
         return None
-        
-
-    
 
     file.close()
     return cronograma
 
-LerArquivoTexto("TestFile.txt")
+def LerArquivoCSV(nomeDoArquivo):
+    dirPath = os.path.dirname(os.path.abspath(__file__))
+    folderPath = 'GeneralData'
+    filePath = os.path.join(dirPath, folderPath)
+    filePath = os.path.join(filePath, nomeDoArquivo)
+    contadorLinhas = 0
+
+    try:
+        with open(filePath, "r") as file:
+            csvFile = csv.reader(file)
+            cronograma = []
+
+
+            for lines in csvFile:
+                contadorLinhas += 1 
+                if(contadorLinhas > 1):
+                    if len(lines) != 5:
+                        raise Exception("A quantidade de campos preenchidos na linha " + str(contadorLinhas) +  " esta incorreta")
+                    if ';' in  lines[1]:
+                        predecessors = lines[1]
+                        predecessorsArray = predecessors.rsplit(";")
+                        lines[1] = predecessorsArray
+                    if ';' in lines[2]:
+                        successors = lines[2]
+                        successorsArray = successors.rsplit(";")
+                        lines[2] = successorsArray
+                    
+                    cronograma.append(lines)
+    except FileNotFoundError:
+        ErroCSVnotFound(NomeDoArquivo=nomeDoArquivo)
+        return None
+    
+    file.close()
+    return cronograma
