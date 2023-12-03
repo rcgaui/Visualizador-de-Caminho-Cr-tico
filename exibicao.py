9fd4cfff443afb680e3a7d31a7e9b24fb8b844ec
Vagner Messias <vagnermessias.vmc@gmail.com>
2023-12-03 08:24:53 -0300

    Inicio ValidaCSV + arquiivo grupo mancala

--- exibicao.py
+++ exibicao.py
@@ -1,10 +1,17 @@
+import outputformat as ouf
 
 def ErroTXTnotFound(NomeDoArquivo):
     errorMsg = "Desculpe, o arquivo " + NomeDoArquivo + " nao foi encontrado na pasta GeneralData."
     print(errorMsg)
 
-
-
 def ErroCSVnotFound(NomeDoArquivo):
     errorMsg = "Desculpe, o arquivo " + NomeDoArquivo + " nao foi encontrado na pasta GeneralData."
     print(errorMsg)
+
+def erroNumeroLinhas(linhas):
+    errorMsg = "A quantidade de campos preenchidos na linha " + str(linhas) +  " esta incorreta"
+    print(errorMsg)
+
+def exibirCaminhoCritico():
+    data = ["Tarefa 1", "Tarefa 7", "Tarefa 8", "Tarefa 11"]
+    ouf.showlist(data, style="box", title="Caminho Critico")
--- leituradados.py
+++ leituradados.py
@@ -1,71 +1,46 @@
 import os
 from exibicao import *
-import csv
-
-def LerArquivoTexto(nomeDoArquivo):
-    dirPath = os.path.dirname(os.path.abspath(__file__))
-    folderPath = 'GeneralData'
-    filePath = os.path.join(dirPath, folderPath)
-    filePath = os.path.join(filePath, nomeDoArquivo)
-    contadorLinhas = 0
-
-    try:
-        with open(filePath, "r") as file:
-            cronograma = []
-
-            for task in file:
-                contadorLinhas += 1
-                tasksArray = task.split()
-                if len(tasksArray) != 5:
-                    raise Exception("A quantidade de campos preenchidos na linha " + str(contadorLinhas) +  " esta incorreta")
-                if '[' in  tasksArray[1]:
-                    predecessors = tasksArray[1][1:-1]
-                    predecessorsArray = predecessors.rsplit(";")
-                    tasksArray[1] = predecessorsArray
-                if '[' in tasksArray[2]:
-                    successors = tasksArray[2][1:-1]
-                    successorsArray = successors.rsplit(";")
-                    tasksArray[2] = successorsArray
-                
-                cronograma.append(tasksArray)
-    except FileNotFoundError:
-        ErroTXTnotFound(NomeDoArquivo=nomeDoArquivo)
-        return None
-
-    file.close()
-    return cronograma
+import pandas as pd
+from datetime import datetime
 
 def LerArquivoCSV(nomeDoArquivo):
     dirPath = os.path.dirname(os.path.abspath(__file__))
     folderPath = 'GeneralData'
     filePath = os.path.join(dirPath, folderPath)
     filePath = os.path.join(filePath, nomeDoArquivo)
-    contadorLinhas = 0
 
+    pd.options.display.max_rows = 9999
+
+    
     try:
         with open(filePath, "r") as file:
-            csvFile = csv.reader(file)
-            cronograma = []
-
-
-            for lines in csvFile:
-                contadorLinhas += 1 
-                if(contadorLinhas > 1):
-                    if len(lines) != 5:
-                        raise Exception("A quantidade de campos preenchidos na linha " + str(contadorLinhas) +  " esta incorreta")
-                    if ';' in  lines[1]:
-                        predecessors = lines[1]
-                        predecessorsArray = predecessors.rsplit(";")
-                        lines[1] = predecessorsArray
-                    if ';' in lines[2]:
-                        successors = lines[2]
-                        successorsArray = successors.rsplit(";")
-                        lines[2] = successorsArray
-                    
-                    cronograma.append(lines)
+            csvFile = pd.read_csv(file, header=0,usecols=["Tarefa", "Predecessor", "Sucessor", "Data de Inicio", "Data de Término"])
+            validaCSV(csvFile)
+            #chama func palma
     except FileNotFoundError:
         ErroCSVnotFound(NomeDoArquivo=nomeDoArquivo)
         return None
     
     file.close()
-    return cronograma
+
+def validaCSV(Arquivo):
+    contadorLinhas = 0
+    i = 0
+    matriz = Arquivo.shape
+
+    if(matriz[1] != 5):
+        raise Exception(erroNumeroLinhas(contadorLinhas))   
+    while(i<matriz[0]):
+        dataInicio = Arquivo.loc[i].at["Data de Inicio"]
+        dataFim = Arquivo.loc[i].at["Data de Término"]
+
+        if dataInicio != datetime.strptime(dataInicio, "%d/%m/%Y").strftime('%d/%m/%Y') or dataFim != datetime.strptime(dataFim, "%d/%m/%Y").strftime('%d/%m/%Y'):
+                raise ValueError
+                
+        i += 1
+    
+            
+              
+
+
+#LerArquivoCSV("CSVTestFile.csv")
