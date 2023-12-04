1267167f75e324df55b380b814637b526ad6c553
Vagner Messias <vagnermessias.vmc@gmail.com>
2023-12-03 23:21:00 -0300

    Features de Exibicao

--- main.py
+++ main.py
@@ -5,17 +5,23 @@ import outputformat as ouf
 import pandas as pd
 
 
-
+ouf.linetitle("                                                                                                                        ", style="double")
 ouf.bigtitle("calculo caminho critico")
-print("Bem vindo ao projeto de calculo de caminho critico, insira abaixo o nome do arquivo CSV junto com o tipo. Ex: Cronograma.csv\n ")
+ouf.linetitle("                                                                                                                        ", style="double")
+
+ouf.boxtitle("Bem vindo ao projeto de calculo de caminho critico, insira abaixo o nome do arquivo CSV junto com o tipo. Ex: Cronograma.csv", style="double")
 
 arquivo =  input("Qual o nome do arquivo? \n")
 while(arquivo):
+    
     cpm(arquivo)
-    arquivo =  input("Qual o nome do arquivo? \n")
 
     validacao = input("Gostaria de calcular outro caminho critico? (Y/N)")
     if(validacao != "Y"):
         break
+    
+    arquivo =  input("Qual o nome do arquivo? \n")
+
+    
 
 
